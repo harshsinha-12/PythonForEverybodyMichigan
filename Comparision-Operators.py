31a98c8e674a99252dca59e6a22e676dfcf87271
Harsh Sinha <137826496+harshsinha-12@users.noreply.github.com>
2023-07-19 19:12:50 +0530

    Add files via upload

---
+++ Comparision-Operators.py
@@ -0,0 +1,25 @@
+x = 5
+if x == 5:
+    print("Equals to 5")
+if x > 4:
+    print("Greater than 4")
+if x >= 5:
+    print("Greater than equal to 5")
+if x < 6:
+    print("Less than 6")
+if x <= 5:
+    print("Less than equal to 5")
+if x != 6:
+    print("Not equal to 6")
+
+
+if x ==5:
+    print("Is 5")
+
+print("Afterwards 5")
+print("Before 6")
+
+if x == 6:
+    print("Is 6")
+
+print("Afterwards 6")
