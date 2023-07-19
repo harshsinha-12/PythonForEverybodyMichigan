748f6bdbd5811648066392ee50152d34a5ab427a
Harsh Sinha <137826496+harshsinha-12@users.noreply.github.com>
2023-07-19 20:11:48 +0530

    Add files via upload

---
+++ Stored-and-reused-Steps(Function Intro).py
@@ -0,0 +1,6 @@
+def thing():        # Decleration of Function
+    print('Hello World')
+
+thing()              # Calling Function once
+print("zip")
+thing()               # Calling Function for 2nd time
