09d17e002b360e541e4dd8b5c7828aace282dd00
Harsh Sinha <137826496+harshsinha-12@users.noreply.github.com>
2023-07-20 15:04:49 +0530

    Add files via upload

---
+++ Lang_Func.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print("bonjour")
+    else:
+        print("Hello")
+
+lang = input('Enter lang:')
+greet(lang)   # call (Argument)
