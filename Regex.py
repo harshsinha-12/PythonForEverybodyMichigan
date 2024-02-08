80de48bdf627346921de9f0c63d3d8cec1b2ee6f
Harsh Sinha <sinha.harshsep@gmail.com>
2024-02-08 18:25:57 +0530

    UMich

---
+++ Regex.py
@@ -0,0 +1,5 @@
+import re
+
+# re.search() -> returns a match object if there is a match anywhere in the string
+# re.match() -> returns a match object if there is a match at the beginning of the string
+# re.findall() -> returns a list of all matches in the string 
