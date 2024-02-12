7b435ab1592458c6e988761b71a5f318d616c27d
Harsh Sinha <sinha.harshsep@gmail.com>
2024-02-12 14:13:50 +0530

    Add files via upload

---
+++ Getting-Started-with-Python-main/Conditional_Steps.py
@@ -0,0 +1,15 @@
+#1
+x = 5
+if x < 10:
+    print("Smaller")
+if x > 20:
+    print("Bigger")
+
+print("Finis")
+
+#2
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("Blastoff!")
---
+++ Getting-Started-with-Python-main/Fuc_Dict_File_Intro.py
@@ -0,0 +1,21 @@
+# Count words in a file
+
+
+# Read data from user
+name = input("Enter File:")
+handle = open(name, 'r')
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1 # Updating Counts
+
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:     # Finding largest item in list
+        bigword = word
+        bigcount = count
+
+print(bigword, bigcount)
